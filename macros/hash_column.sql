66b32cd5428059c107e8314e5e5887c3b138e568
Ramon <ramon.vermeulen@ing.com>
2023-12-18 11:46:43 +0100

    exercise 3

---
+++ macros/hash_column.sql
@@ -0,0 +1,8 @@
+{% macro hash_columns(columns=[]) %}
+    SHA256(CONCAT(
+        {% for column in columns %}
+        {{ column }}
+        {% if not loop.last %},{% endif %}
+        {% endfor %}
+    ))
+{% endmacro %}
--- models/marts/issuer_risk_report.sql
+++ models/marts/issuer_risk_report.sql
@@ -11,13 +11,21 @@ WITH pre AS (
     LEFT JOIN {{ ref('int_fxspot') }} AS fx
         ON ir.as_of_date = fx.as_of_date
         AND ir.trading_currency = fx.foreign_currency
+),
+
+primary_key_added as (
+    SELECT
+        *,
+        {{ hash_columns(columns=['as_of_date', 'trade_id']) }} as pk
+    FROM pre
 )
 
 SELECT
+    pk,
     as_of_date,
     trade_id,
     trading_currency,
     notional_value,
     price,
     notional_value/price AS notional_value_eur
-FROM pre
+FROM primary_key_added
